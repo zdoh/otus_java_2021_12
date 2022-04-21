80b1cdd8ee5508281378ce40a74b15d6a1c9d67c
kravchenko.denis <denfairycat@gmail.com>
2022-04-21 05:59:35 +0300

    hw05 - переделка

--- hw05-byteCodes/src/main/java/ru/otus/Ioc.java
+++ hw05-byteCodes/src/main/java/ru/otus/Ioc.java
@@ -4,6 +4,8 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import ru.otus.annotation.Log;
 
 public class Ioc {
@@ -24,20 +26,41 @@ public class Ioc {
 
         private final T myClass;
 
-        private final Class<?> impClass;
+        private final List<Method> implMethods;
 
         MyInvocationHandler(T myClass) {
             this.myClass = myClass;
-            this.impClass = myClass.getClass();
+            this.implMethods = Arrays.stream(myClass.getClass().getMethods()).toList();
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object... args) throws Throwable {
-            if (impClass.getDeclaredMethod(method.getName(), method.getParameterTypes())
-                    .isAnnotationPresent(Log.class)) {
+            var implMethod = implMethods.stream()
+                    .filter(iMethod -> compareTwoMethod(iMethod, method))
+                    .findFirst().orElse(null);
+
+            if (Objects.nonNull(implMethod) && implMethod.isAnnotationPresent(Log.class)) {
                 System.out.println("executed method: calculation, param: " + Arrays.toString(args));
             }
+
             return method.invoke(myClass, args);
+        }
+
+        private static boolean compareTwoMethod(Method first, Method second) {
+            // собираем список типов параметров в первом методе
+            var firstMethodParamTypeNames = Arrays
+                    .stream(first.getParameters())
+                    .map(a -> a.getParameterizedType().getTypeName())
+                    .toList();
+
+            // собираем список типов параметров во втором методе
+            var secondMethodParamTypeNames = Arrays
+                    .stream(second.getParameters())
+                    .map(a -> a.getParameterizedType().getTypeName())
+                    .toList();
+
+            return Objects.equals(first.getName(), second.getName()) &&
+                    firstMethodParamTypeNames.equals(secondMethodParamTypeNames);
         }
     }
 }
