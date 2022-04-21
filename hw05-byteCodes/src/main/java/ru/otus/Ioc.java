3411568be342b402b2ebfba03190a8fce492433f
kravchenko.denis <denfairycat@gmail.com>
2022-04-21 10:20:38 +0300

    hw05 - переделка для ускорения

--- hw05-byteCodes/src/main/java/ru/otus/Ioc.java
+++ hw05-byteCodes/src/main/java/ru/otus/Ioc.java
@@ -4,6 +4,10 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 import ru.otus.annotation.Log;
 
 public class Ioc {
@@ -24,20 +28,47 @@ public class Ioc {
 
         private final T myClass;
 
-        private final Class<?> implClass;
+        private final Map<Method, Boolean> implClassMethodContainLogAnnotations;
 
         MyInvocationHandler(T myClass) {
             this.myClass = myClass;
-            this.implClass = myClass.getClass();
+
+            this.implClassMethodContainLogAnnotations =
+                    Arrays.stream(myClass.getClass().getInterfaces())
+                            .map(Class::getMethods)
+                            .map(Arrays::asList)
+                            .flatMap(List::stream)
+                            .collect(Collectors.toMap(method -> method,
+                                    method -> Arrays.stream(myClass.getClass().getMethods()).toList().stream()
+                                            .filter(implMethod -> compareTwoMethod(method, implMethod))
+                                            .anyMatch(implMethod -> implMethod.isAnnotationPresent(Log.class))
+                            ));
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object... args) throws Throwable {
-
-            if (implClass.getMethod(method.getName(), method.getParameterTypes()).isAnnotationPresent(Log.class)) {
+            if (Objects.equals(implClassMethodContainLogAnnotations.get(method), true)) {
                 System.out.println("executed method: calculation, param: " + Arrays.toString(args));
             }
+
             return method.invoke(myClass, args);
         }
+    }
+
+    private static boolean compareTwoMethod(Method first, Method second) {
+        // собираем список типов параметров в первом методе
+        var firstMethodParamTypeNames = Arrays
+                .stream(first.getParameters())
+                .map(a -> a.getParameterizedType().getTypeName())
+                .toList();
+
+        // собираем список типов параметров во втором методе
+        var secondMethodParamTypeNames = Arrays
+                .stream(second.getParameters())
+                .map(a -> a.getParameterizedType().getTypeName())
+                .toList();
+
+        return Objects.equals(first.getName(), second.getName()) &&
+                firstMethodParamTypeNames.equals(secondMethodParamTypeNames);
     }
 }
