f3d620c571e8ecdc9c0a5ba0286e6877d6122cb4
kravchenko.denis <denfairycat@gmail.com>
2022-07-24 23:52:07 +0300

    hw07 - убирает комментарии в тестах и убираем метку todo для 1

--- hw07-structuralPatterns/src/test/java/ru/otus/listener/homework/HistoryListenerTest.java
+++ hw07-structuralPatterns/src/test/java/ru/otus/listener/homework/HistoryListenerTest.java
@@ -14,7 +14,6 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 class HistoryListenerTest {
 
     @Test
-    @Disabled //надо удалить
     void listenerTest() {
         //given
         var historyListener = new HistoryListener();
@@ -33,12 +32,12 @@ class HistoryListenerTest {
 
         //when
         historyListener.onUpdated(message);
-//TODO: раскоментировать        message.getField13().setData(new ArrayList<>()); //меняем исходное сообщение
-//TODO: раскоментировать        field13Data.clear(); //меняем исходный список
+        message.getField13().setData(new ArrayList<>()); //меняем исходное сообщение
+        field13Data.clear(); //меняем исходный список
 
         //then
         var messageFromHistory = historyListener.findMessageById(id);
         assertThat(messageFromHistory).isPresent();
-//TODO: раскоментировать        assertThat(messageFromHistory.get().getField13().getData()).containsExactly(data);
+        assertThat(messageFromHistory.get().getField13().getData()).containsExactly(data);
     }
 }
